a92232e0abca8f3a45af54bb4c9dcdce098dba62
thomasrosen <thomas.rosen@me.com>
2023-01-15 06:49:08 +0000

    thomasrosen published a site update

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -60,7 +60,6 @@ export default function App() {
       search_params_data.lon = longitude
     }
 
-    console.log('tags', tags)
     if (Array.isArray(tags) && tags.length > 0) {
       search_params_data.tags = tags.join(',')
     }
@@ -68,14 +67,40 @@ export default function App() {
     const search_params = new URLSearchParams(search_params_data).toString()
 
     const url = `${window.urls.api}resources.json${search_params.length > 0 ? '?' + search_params : ''}`;
-    console.log(url);
 
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setResources(data.resources)
-        console.log(data.resources)
       })
+  }, [])
+
+  const loadTags = React.useCallback(() => {
+
+    const current_url = new URL(window.location);
+    const latitude = (current_url.searchParams.get('lat') || '')
+    const longitude = (current_url.searchParams.get('lon') || '')
+
+    const search_params_data = {}
+
+    if (
+      typeof latitude === 'string' && latitude.length > 0 &&
+      typeof longitude === 'string' && longitude.length > 0
+    ) {
+      search_params_data.lat = latitude
+      search_params_data.lon = longitude
+    }
+
+    const search_params = new URLSearchParams(search_params_data).toString()
+
+    const url = `${window.urls.api}tags.json${search_params.length > 0 ? '?' + search_params : ''}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setTags(data.tags)
+      })
+
   }, [])
 
   const getLocation = React.useCallback(() => {
@@ -93,6 +118,7 @@ export default function App() {
           ['lon', new_longitude],
         ]))
 
+        loadTags()
         loadResources()
       }, (error) => {
         setError(error.message)
@@ -100,7 +126,7 @@ export default function App() {
     } else {
       setError('Geolocation is not supported by this browser.')
     }
-  }, [loadResources])
+  }, [loadTags, loadResources])
 
   const clearLocation = React.useCallback(() => {
     setLatitude(null)
@@ -109,8 +135,9 @@ export default function App() {
       ['lat', ''],
       ['lon', ''],
     ]))
+    loadTags()
     loadResources()
-  }, [loadResources])
+  }, [loadTags, loadResources])
 
   const toggleTag = React.useCallback(tag => {
 
@@ -139,14 +166,9 @@ export default function App() {
     const selected_tags = (current_url.searchParams.get('tags') || '').split(',')
     setSelectedTags(new Set(selected_tags))
 
-    fetch(window.urls.api + 'tags.json')
-    .then((response) => response.json())
-    .then((data) => {
-      setTags(data.tags)
-    })
-
+    loadTags()
     loadResources()
-  }, [loadResources])
+  }, [loadTags, loadResources])
 
   return <div className="app_wrapper">
     <header>
