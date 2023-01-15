46170cc21f234fc0b3d1ed860407ae447cd4c8d6
thomasrosen <thomas.rosen@me.com>
2023-01-15 08:14:46 +0100

    Update Tag.js

--- frontend/src/components/Tag.js
+++ frontend/src/components/Tag.js
@@ -7,6 +7,7 @@ import {
 
 export default function Tag({ tag, style, ...props }) {
   const color = string2color(tag) // TODO make sure to only run this once
+  // TODO define the colors instead of generating them
   const textColor = getContrastingColorFromHex(color) // TODO make sure to only run this once
 
   return <button
